12951afa87ba8c38a148807d9dbc6635f6f442aa
Fu-Sheng <rich840213@gmail.com>
2019-08-18 20:27:43 +0800

    done test

---
+++ src/config/db.php
@@ -0,0 +1,19 @@
+<?php
+
+class db
+{
+    // Properties
+    private $dbhost = '127.0.0.1';
+    private $dbuser = 'root';
+    private $dbpass = '123456';
+    private $dbname = 'slimapptest';
+
+    // Connect
+    public function connect()
+    {
+        $mysql_connect_str = "mysql:host=$this->dbhost;dbname=$this->dbname";
+        $dbConnection = new PDO($mysql_connect_str, $this->dbuser, $this->dbpass);
+        $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $dbConnection;
+    }
+}
